02a0119953f7b21a4783ebb40f0f10ddce2a78ef
René Calfin Olate <rene.calfin@gmail.com>
2020-01-30 21:19:41 -0300

    Terminando dia 30 enero 2020

---
+++ src/dia30enero/arreglosEjercicio1.java
@@ -0,0 +1,25 @@
+package dia30enero;
+
+import java.util.Scanner;
+
+public class arreglosEjercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cantidad;
+		cantidad =2;
+		String Usuario[];
+		Usuario = new String [cantidad];
+		
+		Usuario[0]= "Pablo, 23, Masculino";
+		Usuario[1]= "Carla, 45, Femenino";
+		for (int i = 0; i < 2; i++) {
+			System.out.printf("\n%s",Usuario[i]);
+			
+		}
+		Scanner Teclado = new Scanner(System.in);
+		
+		
+	}
+
+}
