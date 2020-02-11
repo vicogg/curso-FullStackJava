a5059f5c484983496edbdd01e2e26c39e2b078fe
vicogg <60899938+vicogg@users.noreply.github.com>
2020-02-11 19:13:44 -0300

    Update Proceso.java

--- src/dia11febEjercicio2/Proceso.java
+++ src/dia11febEjercicio2/Proceso.java
@@ -1,6 +1,6 @@
 package dia11febEjercicio2;
 import java.util.Scanner;
-public class Proceso {
+public class Proceso1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
